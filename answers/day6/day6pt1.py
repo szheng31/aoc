8a8e6378aaa32358aeba4953febba7e4d7c48e07
szheng31 <63608979+szheng31@users.noreply.github.com>
2020-12-06 00:15:02 -0500

    Update day6pt1.py

--- answers/day6/day6pt1.py
+++ answers/day6/day6pt1.py
@@ -1,1 +1,21 @@
-
+f = open('input.txt','r')
+input = f.read().split('\n\n')
+print(input)
+newlist = []
+for i in input:
+    k = i.split('\n')
+    
+    newlist.append(''.join(k))
+for z in newlist:
+    print(z)
+numberofyes = 0
+for i in newlist:
+    listofstuff = []
+    for z in i:
+        
+        if z in listofstuff:
+            pass
+        else:
+            listofstuff.append(z)
+            numberofyes += 1
+print(numberofyes)
