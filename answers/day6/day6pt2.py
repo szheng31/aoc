bb6a5c5d0a82c518dfac2b8f686d6a1a088859bb
szheng31 <63608979+szheng31@users.noreply.github.com>
2020-12-06 00:52:23 -0500

    Update day6pt2.py

--- answers/day6/day6pt2.py
+++ answers/day6/day6pt2.py
@@ -1,1 +1,43 @@
+f = open('input.txt','r')
+input = f.read().split('\n\n')
 
+newlist = []
+listoflistvalues = []
+answer = 0
+for i in input:
+    k = i.split('\n')
+    
+    
+    newlist.append(k)
+
+
+for i in newlist:
+    
+    k = ''.join(i)
+    listofstuff = []
+    for z in k:
+        
+        if z in listofstuff:
+            
+            pass
+        else:
+            listofstuff.append(z)
+            
+            
+    
+    for b in listofstuff:
+        counter = 0
+        
+        for x in i:
+            
+            if b in x:
+                counter += 1
+            else:
+                break
+        if counter == len(i):
+            answer += 1
+
+
+    
+    
+print(answer)
