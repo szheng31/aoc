b7ad72cb36e8cb09ed66d7a701d9e9f580013209
Stanley Zheng <63608979+szheng31@users.noreply.github.com>
2020-12-12 00:36:21 -0500

    Create day12pt1.py

---
+++ answers/day12/day12pt1.py
@@ -0,0 +1,59 @@
+f = open('input.txt','r')
+actions = f.read().split('\n')
+directionlist = ['N','E','S','W']
+
+fowarddirectionindex = 1
+fowarddirection = directionlist[fowarddirectionindex]
+northcounter = 0
+southcounter = 0
+eastcounter = 0
+westcounter = 0
+for i in actions:
+    
+    direction = i[0]
+    distance = int(i[1:])
+    if direction == 'F':
+        if fowarddirection == 'N':
+            northcounter += distance
+        elif fowarddirection == 'S':
+            southcounter += distance
+        elif fowarddirection == 'E':
+            eastcounter += distance
+        elif fowarddirection == 'W':
+            westcounter += distance
+        
+    elif direction == 'N':
+        northcounter += distance
+
+    elif direction == 'S':
+        southcounter += distance
+
+    elif direction == 'E':
+        eastcounter += distance
+
+    elif direction == 'W':
+        westcounter += distance
+
+    elif direction == 'L':
+        index = distance / 90
+        if index == 1:
+            index = 3
+        elif index == 3:
+            index = 1
+        elif index == 4:
+            index = 0
+        fowarddirectionindex = int(index + fowarddirectionindex)
+        if fowarddirectionindex >= len(directionlist):
+            fowarddirectionindex -= 4
+        fowarddirection = directionlist[fowarddirectionindex]
+        
+    elif direction == 'R':
+        index = distance / 90
+        
+        fowarddirectionindex = int(index + fowarddirectionindex)
+        if fowarddirectionindex >= len(directionlist):
+            fowarddirectionindex -= 4
+        fowarddirection = directionlist[fowarddirectionindex]
+print(northcounter,southcounter,eastcounter,westcounter)
+print(abs(northcounter - southcounter))
+print(abs(eastcounter - westcounter))
